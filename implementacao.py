7f6f1383db1cdd3afe4a3f8aa8972c60b729d8be
Diego Ribeiro <diegoribeiro@alu.ufc.br>
2023-11-17 19:18:07 -0300

    Algoritmo para identificar o vértice de maior grau de saída
    Foi implementado o algoritmo para ler o arquivo txt e retornar uma lista dos vértices do maior grau de saída para o menor.

--- implementacao.py
+++ implementacao.py
@@ -7,10 +7,31 @@ def printList(d, f, n):
     # Saída mais organizada do vetores de tempo D e F
     # JOIN concatena cada iteração da lista usando MAP com , e um espaço
 
+#Função para carregar os vértices de maior grau de saída pro menor.
+def listaDosMaioresGraus():
+    file = open("grafos/g2.txt", "r")
+    list = file.readlines()
+    
+    #Irá ler a primeira coluna do arquivo txt do grafo escolhido.
+    for i in range(len(list)):
+        line = list[i].split()
+        if i == 0:
+            n = int(line[0])
+            saidas = [0] * n #Crio um vetor de grau de saída de acordo com o N de vértices.
+        else:
+            saidas[int(line[0])-1] += 1 #Cada vez que o número aparecer, quer dizer que seu grau de saída é aumentado.
+    file.close()
+
+    maioresgraus = [] #Crio uma lista vazia para armazenas os vértices do maior grau pro menor.
+    for i in range(len(saidas)):
+        maioresgraus.append(saidas.index(max(saidas))) #Vai adicionando na lista o índice(vértice) de maior grau até então.
+        saidas[saidas.index(max(saidas))] = -1 #E no local do vértice de maior grau de saída é substituido por -1, para simbolizar que o vértice já foi removido da lista.
+    return maioresgraus #E retorna a lista com os vértices do maior grau de saída pro menor.
+    
 
 #Função para carregar a lista de adjacentes
 def loadList():
-    file = open("grafos/g1.txt", "r") #Abro o arquivo txt em modo de leitura
+    file = open("grafos/g2.txt", "r") #Abro o arquivo txt em modo de leitura
     list = file.readlines() #Leio cada linha
 
     for i in range(len(list)):
@@ -25,7 +46,6 @@ def loadList():
     
     file.close()
     return list_adjacent, n
-
 
 
 def DFS_visit(u):
@@ -66,11 +86,10 @@ def exibirTiposDasArestas():
     for i in tiposDasArestas:
         print(i)
 
-tiposDasArestas = []
 
+tiposDasArestas = [] #Lista que irá armazenar a nomenclatura das arestas.
 [lista_adj, n] = loadList() #Carrego a lista de adjacência e o tamanho de vertices.
-
-v = [1,0,2,3,4,5,6,7] #Ordem de vértices que o algoritmo irá ser executado.
+v = listaDosMaioresGraus() #Ordem de vértices de maior pro menor grau de saída que o algoritmo irá ser executado.
 
 #Vetores iniciais e a variável de marcação inicializados.
 cor = [0] * n 
